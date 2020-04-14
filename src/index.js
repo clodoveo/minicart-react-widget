f1aeea201ed7d8fcfeef08dff2b447587da06f75
clodoveo <filippo@ramointeractive.com>
2020-04-14 10:36:59 +0200

    sposto componenti dentro src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-import Lista from "/components/Lista";
-import Footer from "/components/Footer";
+import Lista from "./components/Lista";
+import Footer from "./components/Footer";
 
 import "./styles.css";
 
